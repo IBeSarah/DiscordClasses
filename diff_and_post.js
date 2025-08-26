8b6af12caa2c11a9f8e5d44fe26969ff17be81b6
StilLTheSameAccount <139647336+StilLTheSameAccount@users.noreply.github.com>
2025-08-26 01:14:34 -0500

    Update diff_and_post.js

--- diff_and_post.js
+++ diff_and_post.js
@@ -1,120 +1,100 @@
-// diff_and_post.js
 const fs = require('fs');
+const { Octokit } = require('@octokit/rest');
 const path = require('path');
-const axios = require('axios');
+const axios = require('axios'); // for Discord webhook
 
-const MAX_DISCORD_LENGTH = 2000;
-const commitUrl = process.env.GITHUB_SERVER_URL + '/' + process.env.GITHUB_REPOSITORY + '/commit/' + process.env.GITHUB_SHA;
+// GitHub Actions environment
+const githubToken = process.env.GITHUB_TOKEN;
+const commitSha = process.env.GITHUB_SHA;
+const repo = process.env.GITHUB_REPOSITORY; // owner/repo
+const commitUrl = `https://github.com/${repo}/commit/${commitSha}`;
+const discordWebhookUrl = process.env.DISCORD_WEBHOOK_URL; // optional
 
-const prevFile = path.join(__dirname, 'previous.json');
-const currFile = path.join(__dirname, 'current.json');
-const mainFile = path.join(__dirname, 'discordclasses.json');
+const prevPath = path.join(__dirname, 'previous.json');
+const currPath = path.join(__dirname, 'current.json');
 
-// Normalize JSON by sorting keys recursively
-function normalize(obj) {
-  if (Array.isArray(obj)) return obj.map(normalize);
-  if (obj && typeof obj === 'object') {
-    return Object.keys(obj)
-      .sort()
-      .reduce((res, key) => {
-        res[key] = normalize(obj[key]);
-        return res;
-      }, {});
-  }
-  return obj;
+function readJsonSafe(filePath) {
+  if (!fs.existsSync(filePath)) return {};
+  try { return JSON.parse(fs.readFileSync(filePath, 'utf8')); }
+  catch { return {}; }
 }
 
-function readJSON(filePath) {
-  try {
-    const data = fs.readFileSync(filePath, 'utf8');
-    return JSON.parse(data);
-  } catch {
-    return {};
-  }
-}
-
-function writeJSON(filePath, data) {
-  fs.writeFileSync(filePath, JSON.stringify(normalize(data), null, 2), 'utf8');
-}
-
-// Diffing function
-function diffObjects(prev, curr, prefix = '') {
-  const added = [];
-  const removed = [];
-  const renamed = [];
-  const moved = [];
-
-  const prevKeys = Object.keys(prev || {});
-  const currKeys = Object.keys(curr || {});
-
-  for (const key of currKeys) {
-    const fullKey = prefix ? `${prefix}.${key}` : key;
-    if (!(key in prev)) {
-      added.push(fullKey);
-    } else if (
-      typeof curr[key] === 'object' &&
-      curr[key] !== null &&
-      typeof prev[key] === 'object' &&
-      prev[key] !== null
-    ) {
-      const { added: a, removed: r, renamed: rn, moved: m } = diffObjects(prev[key], curr[key], fullKey);
-      added.push(...a);
-      removed.push(...r);
-      renamed.push(...rn);
-      moved.push(...m);
-    } else if (curr[key] !== prev[key]) {
-      renamed.push(fullKey);
-    }
-  }
-
-  for (const key of prevKeys) {
-    const fullKey = prefix ? `${prefix}.${key}` : key;
-    if (!(key in curr)) {
-      removed.push(fullKey);
-    }
-  }
-
+function diffObjects(prev, curr) {
+  const added = [], removed = [], renamed = [], moved = [];
+  const allModules = new Set([...Object.keys(prev), ...Object.keys(curr)]);
+  allModules.forEach(moduleKey => {
+    const prevModule = prev[moduleKey] || {};
+    const currModule = curr[moduleKey] || {};
+    const allKeys = new Set([...Object.keys(prevModule), ...Object.keys(currModule)]);
+    allKeys.forEach(k => {
+      if (!(k in prevModule)) added.push({ module: moduleKey, key: k });
+      else if (!(k in currModule)) removed.push({ module: moduleKey, key: k });
+      else if (prevModule[k] !== currModule[k]) renamed.push({ module: moduleKey, key: k });
+    });
+  });
   return { added, removed, renamed, moved };
 }
 
-function formatDiffSection(title, keys, symbol = '+') {
-  if (!keys.length) return '';
-  return `### ${title}\n${keys.map(k => `${symbol} ${k}`).join('\n')}\n`;
+function formatDiff(diffObj) {
+  const lines = [];
+  if (diffObj.removed.length) {
+    lines.push('### Removed');
+    diffObj.removed.forEach(r => lines.push(`- ${r.key} removed from Module ${r.module}`));
+  }
+  if (diffObj.added.length) {
+    lines.push('### Added');
+    diffObj.added.forEach(a => lines.push(`+ ${a.key} added to Module ${a.module}`));
+  }
+  if (diffObj.renamed.length) {
+    lines.push('### Renamed');
+    diffObj.renamed.forEach(rn => lines.push(`~ ${rn.key} renamed in Module ${rn.module}`));
+  }
+  if (diffObj.moved.length) {
+    lines.push('### Moved');
+    diffObj.moved.forEach(mv => lines.push(`> ${mv.key} moved in Module ${mv.module}`));
+  }
+  return lines.join('\n');
 }
 
-(async () => {
-  // Normalize the main JSON file
-  const mainJSON = readJSON(mainFile);
-  writeJSON(mainFile, mainJSON);
-
-  // Read normalized previous/current
-  const prevJSON = readJSON(prevFile);
-  const currJSON = readJSON(currFile);
-
-  const { added, removed, renamed, moved } = diffObjects(prevJSON, currJSON);
-
-  if (!added.length && !removed.length && !renamed.length && !moved.length) {
-    console.log('No changes detected.');
-    return;
+async function postGitHubComment(octokit, body) {
+  const MAX_LENGTH = 65000; // GitHub max per comment
+  const chunks = [];
+  for (let i = 0; i < body.length; i += MAX_LENGTH) {
+    chunks.push(body.slice(i, i + MAX_LENGTH));
   }
-
-  let message = '```diff\n';
-  message += formatDiffSection('Removed', removed, '-');
-  message += formatDiffSection('Added', added, '+');
-  message += formatDiffSection('Renamed', renamed, '~');
-  message += formatDiffSection('Moved', moved, '>');
-  message += '```\n';
-  message += `Full details here: ${commitUrl}`;
-
-  if (message.length > MAX_DISCORD_LENGTH) {
-    message = message.slice(0, MAX_DISCORD_LENGTH - 3) + '...';
+  for (const chunk of chunks) {
+    await octokit.rest.repos.createCommitComment({
+      owner: repo.split('/')[0],
+      repo: repo.split('/')[1],
+      commit_sha: commitSha,
+      body: chunk
+    });
   }
+}
 
-  try {
-    await axios.post(process.env.DISCORD_WEBHOOK_URL, { content: message });
-    console.log('Posted summarized diff to Discord successfully.');
-  } catch (err) {
-    console.error('Failed to send Discord webhook:', err.message);
-    process.exit(1);
-  }
-})();
+async function postDiscordComment(body) {
+  if (!discordWebhookUrl) return;
+  const MAX_LENGTH = 2000 - 100;
+  const msg = body.length > MAX_LENGTH ? body.slice(0, MAX_LENGTH) + '\n...' : body;
+  await axios.post(discordWebhookUrl, { content: '```diff\n' + msg + '\n```\nFull commit here: ' + commitUrl });
+}
+
+async function main() {
+  const prevJson = readJsonSafe(prevPath);
+  const currJson = readJsonSafe(currPath);
+  const diffObj = diffObjects(prevJson, currJson);
+  const formattedDiff = formatDiff(diffObj);
+
+  if (!formattedDiff) return console.log('No changes detected');
+
+  const githubOctokit = new Octokit({ auth: githubToken });
+  const fullGitHubComment = formattedDiff + '\nFull commit here: ' + commitUrl;
+
+  await postGitHubComment(githubOctokit, fullGitHubComment);
+  console.log('GitHub comment posted.');
+
+  await postDiscordComment(formattedDiff);
+  console.log('Discord comment posted.');
+}
+
+main().catch(err => console.error(err));
