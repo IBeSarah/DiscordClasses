c33186dac633a149cd9b521e4dc014e0d170af66
StilLTheSameAccount <139647336+StilLTheSameAccount@users.noreply.github.com>
2025-08-26 21:31:02 -0500

    Update diff_and_post.js

--- diff_and_post.js
+++ diff_and_post.js
@@ -4,19 +4,32 @@ const axios = require('axios');
 const levenshtein = require('fast-levenshtein');
 const { Octokit } = require('@octokit/rest');
 
-const fileName = process.argv[2];
-if (!fileName) throw new Error("Please provide a filename as an argument.");
+const fileName = process.argv[2] || 'discordclasses.json';
+const prevFile = `previous_${fileName}`;
+const currFile = `current_${fileName}`;
 
-const prev = JSON.parse(require('child_process').execSync(`git show HEAD^:${fileName}`).toString());
-const curr = JSON.parse(require('child_process').execSync(`git show HEAD:${fileName}`).toString());
+// Read previous and current JSON files directly
+let prev = {};
+let curr = {};
+try {
+  prev = JSON.parse(fs.readFileSync(prevFile, 'utf8'));
+} catch (e) {
+  console.warn(`Could not read ${prevFile}, using empty object`);
+}
+try {
+  curr = JSON.parse(fs.readFileSync(currFile, 'utf8'));
+} catch (e) {
+  console.warn(`Could not read ${currFile}, using empty object`);
+}
 
+// Track changes
 const addedModules = {};
 const removedModules = {};
 const renamedModules = {};
 const movedModules = {};
 const usedCurr = new Set();
 
-// Detect moved modules
+// Detect module moves
 for (const oldId of Object.keys(prev)) {
   if (curr[oldId]) continue;
   let bestMatch = null;
@@ -48,7 +61,7 @@ for (const oldId of Object.keys(prev)) {
   }
 }
 
-// Added, Removed, Renamed keys
+// Added, removed, renamed keys
 for (const key of new Set([...Object.keys(prev), ...Object.keys(curr)])) {
   const oldMod = prev[key] || {};
   const newMod = curr[key] || {};
@@ -57,12 +70,10 @@ for (const key of new Set([...Object.keys(prev), ...Object.keys(curr)])) {
     addedModules[key] = addedModules[key] || [];
     addedModules[key].push(k);
   }
-
   for (const k of Object.keys(oldMod)) if (!newMod.hasOwnProperty(k)) {
     removedModules[key] = removedModules[key] || [];
     removedModules[key].push(k);
   }
-
   for (const k of Object.keys(newMod)) if (oldMod[k] && oldMod[k] !== newMod[k]) {
     renamedModules[key] = renamedModules[key] || [];
     renamedModules[key].push(k);
@@ -89,6 +100,7 @@ for (const [mod, keys] of Object.entries(addedModules)) githubOutput += formatDi
 for (const [mod, keys] of Object.entries(removedModules)) githubOutput += formatDiffBlock('Removed', mod, keys, 'removed');
 for (const [mod, keys] of Object.entries(renamedModules)) githubOutput += formatDiffBlock('Renamed', mod, keys, 'renamed');
 for (const [mod, info] of Object.entries(movedModules)) githubOutput += formatDiffBlock('Moved', mod, info.keys, 'moved', info.to);
+
 if (!githubOutput) githubOutput = 'No changes detected.';
 
 const MAX_COMMENT_LENGTH = 65000;
@@ -102,10 +114,10 @@ function splitText(text, maxLength) {
   return chunks;
 }
 
-// Post full diff as GitHub commit comments
+// Post full diff to GitHub
 (async () => {
-  const octokit = new Octokit({ auth: process.env.GITHUB_TOKEN });
   const githubChunks = splitText(githubOutput, MAX_COMMENT_LENGTH);
+  const octokit = new Octokit({ auth: process.env.GITHUB_TOKEN });
   for (let i = 0; i < githubChunks.length; i++) {
     const body = githubChunks.length > 1 ? `**Part ${i+1} of ${githubChunks.length}**\n\n${githubChunks[i]}` : githubChunks[i];
     await octokit.rest.repos.createCommitComment({
@@ -117,30 +129,17 @@ function splitText(text, maxLength) {
   }
 
   // Discord summary
+  const mods = [...Object.entries(addedModules), ...Object.entries(removedModules), ...Object.entries(renamedModules), ...Object.entries(movedModules)];
+  const summaryLines = mods.slice(0,5).map(([mod, keys]) => {
+    if (keys.to) return `Module ${mod}: Moved: ${keys.keys.length}`;
+    return `Module ${mod}: ${keys.length}`;
+  });
+
+  const remainingCount = mods.length - 5;
+  if (remainingCount > 0) summaryLines.push(`${remainingCount} more changes not included`);
+
   const commitUrl = `https://github.com/${process.env.GITHUB_REPOSITORY}/commit/${process.env.GITHUB_SHA}`;
-  if (fileName === 'base64entries.json') {
-    const addedCount = Object.values(addedModules).reduce((acc, keys) => acc + keys.length, 0);
-    const removedCount = Object.values(removedModules).reduce((acc, keys) => acc + keys.length, 0);
-    const modifiedCount = Object.values(renamedModules).reduce((acc, keys) => acc + keys.length, 0) +
-                          Object.values(movedModules).reduce((acc, info) => acc + info.keys.length, 0);
-    const discordMessage = `### Base64 entries summary\nAdded: ${addedCount}, Removed: ${removedCount}, Modified: ${modifiedCount}\nSee full list of changes here: ${commitUrl}`;
-    await axios.post(process.env.DISCORD_WEBHOOK_URL, { content: discordMessage });
-    console.log('Discord post for base64entries.json successful');
-  } else {
-    const summaryLines = [];
-    const allModules = { addedModules, removedModules, renamedModules, movedModules };
-    for (const [title, mods] of Object.entries(allModules)) {
-      if (Object.keys(mods).length === 0) continue;
-      summaryLines.push(`${title.replace('Modules','')}: ${Object.keys(mods).length} modules changed`);
-    }
-
-    let topModules = Object.keys(curr).slice(0, 5);
-    let extraModules = Object.keys(curr).length - 5;
-    const summary = topModules.map(m => `Module ${m}`).join('\n') +
-                    (extraModules > 0 ? `\n${extraModules} more modules not included` : '');
-
-    const discordMessage = `### Module changes summary\n${summary}\nSee full list of changes here: ${commitUrl}`.slice(0, 2000);
-    await axios.post(process.env.DISCORD_WEBHOOK_URL, { content: discordMessage });
-    console.log('Discord post for discordclasses.json successful');
-  }
+  const discordMessage = `**Module changes summary**\n${summaryLines.join('\n')}\n\nSee full list of changes here: ${commitUrl}`;
+  await axios.post(process.env.DISCORD_WEBHOOK_URL, { content: discordMessage });
+  console.log('Discord post successful');
 })();
